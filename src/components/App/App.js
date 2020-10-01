dba15cc061c5bf500ede7094ca1d20630a865d5c
Roman34 <drowlands60@gmail.com>
2020-10-01 18:43:18 +0100

    Ravenous Part 2 Complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,29 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import logo from '../../logo.svg';
 
+//Business Object
+let business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+//Business Array
+let businessArr = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 
 class App extends React.Component {
   render() {
@@ -11,7 +34,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList businesses={businessArr} /> 
       </div>
   )};
 }
