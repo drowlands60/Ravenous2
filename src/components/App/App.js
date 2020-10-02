453db96ea7142f6108f27fea1c29b65a7bd841de
Roman34 <drowlands60@gmail.com>
2020-10-02 18:38:36 +0100

    Ravenous Stage 3 finito

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,11 +29,16 @@ let businessArr = [
 
 
 class App extends React.Component {
+  
+  searchYelp(term, location, sortBy) {
+    console.log(`looking for ${term} in ${location}, ${sortBy}`)
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businessArr} /> 
       </div>
   )};
